06144c3c7400a6f7bcfc2a42dbf1f3992fea621c
IbtisaamButt <45636328+IbtisaamButt@users.noreply.github.com>
2020-12-31 01:10:46 +0500

    Update helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -14,14 +14,17 @@ class B {
 class C {
  public static void main(String args[], 
                          int asd,
-                         int add, int cda,
+                         int zxc,
+                         int add,
+                         int cda,
                         int dfa,
                         int fga,
-                        int gha){
+                        int gha,
+                        int abc){
      System.out.println("Hello Worldasd");  
  }
 }
 
-int main(int x){
+int main(int x, int fgh){
  return 0;
 }
